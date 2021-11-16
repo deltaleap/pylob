926ffe98e2039788c76890414d5ff8ab203fd672
Mattia Terenzi <m.terenzi92@gmail.com>
2021-11-16 23:54:13 +0100

    fix in trades++

--- pylob/trades/trades.h
+++ pylob/trades/trades.h
@@ -6,14 +6,16 @@ typedef uint32_t quantity_t;
 typedef uint32_t timestamp_t;
 
 struct trade_t {
-    timestamp_t timestamp
+    timestamp_t timestamp;
     price_t price;
     quantity_t quantity;
     uint32_t direction;
+    
 };
 
 struct Trades {
     std::string name;
+    int len;
 
     trade_t last_trade;
 
